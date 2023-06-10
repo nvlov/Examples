8fa3e36ba07c968f2d06956e2e44cdbbf391ac38
Nikolay <nlovtsevich@gmail.com>
2023-06-10 20:54:36 +0300

    add Example006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿// про гирьки
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 8;
+int i = 5;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (i > max) max = i;
+
+Console.Write("max = ");
+Console.WriteLine(max); 
